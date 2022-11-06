2b796b85d7b3639f92cb21007677224909d3f73c
Nicholas Osaka <nicholas.osaka@gmail.com>
2022-11-05 20:01:03 -0400

    Update width of link module for mobile

--- components/LinkModule.tsx
+++ components/LinkModule.tsx
@@ -10,7 +10,7 @@ type LinkData = {
 const Link = (props: LinkData) => {
     const {name, url, blurb} = props
     return (
-        <div className='transition ease-in-out duration-500 hover:scale-110 my-3 mx-auto border-4 rounded-xl py-3 border-jet hover:border-chestnut text-jet hover:text-chestnut w-2/3 md:w-3/5 lg:w-1/3'>
+        <div className='transition ease-in-out duration-500 hover:scale-110 my-3 mx-auto border-4 rounded-xl py-3 border-jet hover:border-chestnut text-jet hover:text-chestnut w-11/12 sm:w-5/6 md:w-3/5 lg:w-1/3'>
             <a href={url} target='_blank' rel="noreferrer">
                 <h1 className='text-xl font-bold tracking-tight text-center'>{name}</h1>
             </a>
